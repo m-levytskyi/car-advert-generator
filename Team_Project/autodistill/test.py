710a54cb505742997245ad61e76e7233e73658e0
JohannesDecker95 <jo.de@online.de>
2024-10-15 00:49:04 +0200

    ...

--- Team_Project/autodistill/test.py
+++ Team_Project/autodistill/test.py
@@ -8,7 +8,7 @@ import torch
 
 # Check if MPS (Metal Performance Shaders) is available for PyTorch
 # device = torch.device('mps') if torch.backends.mps.is_available() else(torch.device('cuda') if torch.backends.cpu.is_available() else torch.device('cpu'))
-torch.set_default_device('mps')
+# torch.set_default_device('mps')
 
 HOME = os.getcwd()
 print("current working directory: " + str(HOME))
@@ -22,18 +22,20 @@ base_model = GroundedSAM(ontology=CaptionOntology({"brand": "brand",
                                                    "model": "model",
                                                    "vehicle class": "body_style",
                                                    "segment": "segment",
-                                                   "engine": "engine_specs_title",
-                                                   "cylinders": "cylinders",
-                                                   "displacement": "displacement",
-                                                   "torque": "torque",
-                                                   "fuel": "fuel",
-                                                   "drive type": "drive_type",
-                                                   "gearbox": "gearbox",
-                                                   "weight": "unladen_weight",
-                                                   "wheelbase": "wheelbase",
-                                                   "cargo volume": "cargo_volume",
-                                                   "top speed": "top_speed",
-                                                   "market launch": "from_year"}))
+                                                   # "drive type": "drive_type",
+                                                   # "engine": "engine_specs_title",
+                                                   # "cylinders": "cylinders",
+                                                   # "displacement": "displacement",
+                                                   # "torque": "torque",
+                                                   # "fuel": "fuel",
+                                                   # "drive type": "drive_type",
+                                                   # "gearbox": "gearbox",
+                                                   # "weight": "unladen_weight",
+                                                   # "wheelbase": "wheelbase",
+                                                   # "cargo volume": "cargo_volume",
+                                                   # "top speed": "top_speed",
+                                                   # "market launch": "from_year"
+                                                   }))
 
 # DATASET_DIR_PATH = f"{HOME}/dataset"
 # IMAGE_DIR_PATH = f"{HOME}/images/"
