a29406b2ab13617e0e823f5b060cd679a9b1f1d4
Simon Hampp <shampp@hm.edu>
2024-11-23 18:24:29 +0100

    filter out markdown, add fields to json

--- Code/article_agent/agent_pipeline.py
+++ Code/article_agent/agent_pipeline.py
@@ -5,10 +5,24 @@ from article_agent import ArticleAgent
 import json
 
 
+def filter_out_markdown(text: list[str]) -> list[str]:
+    """
+    Filter out markdown characters from the text.
+
+    Args:
+        text (list[str]): The text from which markdown characters need to be filtered out.
+
+    Returns:
+        str: The text after filtering out the markdown characters.
+    """
+    return [t.replace('**', '').replace('`', '').replace('"', '') for t in text]
+
+
 class AgentPipeline:
     def __init__(self, brand: str, car_type=None, 
                  instruction: str='Write a sensational paragraph for an advertisement about a car brand based on the information provided.'):
         self.brand = brand
+        self.car_type = car_type
         self.tasks = self.create_tasks(brand, car_type)
         self.instruction = instruction
         self.agent = ArticleAgent()
@@ -42,28 +56,33 @@ class AgentPipeline:
             f"Discuss the innovations of the car {brand}."
         ]
         return tasks
+    
 
     # make it return a json object
     def __call__(self):
         logging.info(f"Article Agent: Creating paragraphs, image descriptions and subtitles for the tasks: {self.tasks}")
-        paragraphs = self.agent.create_paragraphs(self.tasks, self.instruction)
-        image_descriptions = self.agent.create_image_descriptions(paragraphs=paragraphs)
-        subtitles = self.agent.create_image_subtitles(descriptions=image_descriptions)
+        paragraphs = filter_out_markdown(self.agent.create_paragraphs(self.tasks, self.instruction))
+        image_descriptions = filter_out_markdown(self.agent.create_image_descriptions(paragraphs=paragraphs))
+        subtitles = filter_out_markdown(self.agent.create_image_subtitles(descriptions=image_descriptions))
+
         logging.info('Article Agent: Created finished creating paragraphs, image descriptions and subtitles.')
         return json.dumps({
             'paragraphs': paragraphs,
             'captions': subtitles,
-            'prompts': image_descriptions
+            'prompts': image_descriptions,
+            'brand': self.brand,
+            'car_type': self.car_type
         })
 
 
 if __name__ == "__main__":
-    # TODO: autos von anderen Seiten zeigen
-    agent_pipeline = AgentPipeline(brand='Ford', car_type='Hatchback')
+    brand = 'Toyota'
+    car_type = 'Coupe'
+    agent_pipeline = AgentPipeline(brand=brand, car_type=car_type)
     response = agent_pipeline()
     # save json object to a file
 
-    with open('Code/article_agent/json/output.json', 'w') as f:
+    with open(f"Code/article_agent/json/output_{brand}_{car_type}.json", "w") as f:
         f.write(response)
 
 
