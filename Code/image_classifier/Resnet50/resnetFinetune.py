eb21c6e76fee66bcd70d74eea0535f665a3c1d08
Simon Hampp <shampp@hm.edu>
2024-11-26 14:57:34 +0100

    Resnet with finetuning instead of freezing

---
+++ Code/image_classifier/Resnet50/resnetFinetune.py
@@ -0,0 +1,21 @@
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as functional
+
+class ResnetFinetuning(nn.Module):
+    def __init__(self,amount_classes):
+        super().__init__()
+
+        self.epochs=50
+        self.batchsize=64
+        self.loss=functional.cross_entropy
+                
+        self.model = torchvision.models.resnet50(weights='IMAGENET1K_V1')
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, amount_classes)
+
+        self.optimizer=optim.Adam(self.model.fc.parameters(), lr=0.001, weight_decay=0.0001)
+
+    def forward(self,x):
+        return self.model(x)
