4e242ed998e45d645a56f5a431784ecb2a45b6fa
Marvin Wolf <marvin.wolf@hm.edu>
2024-11-30 19:22:37 +0100

    adapted dataloader to filter labels, train for different labels and train with equally distributed data

--- Code/image_classifier/dataloader.py
+++ Code/image_classifier/dataloader.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import os
-from PIL import Image
 from torchvision import  transforms
 from torch.utils.data import Dataset
 import tqdm
@@ -9,7 +8,7 @@ import cv2
 import numpy as np
 import torch
 
-def load_single_image(row, label_map):
+def load_single_image(row, label_map,tolabel_row):
     path_to_img = os.path.join("../../", row.iloc[6])
     if not os.path.exists(path_to_img):
         raise Exception(f"File not found: {path_to_img}")
@@ -29,18 +28,30 @@ def load_single_image(row, label_map):
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     image = normalize(image)
 
-    label = label_map[row.iloc[0]]
+    label = label_map[row[tolabel_row]]
 
     return image, label
 
 class CustomCarDataset(Dataset):
-    def __init__(self, csv_file, phase='train', in_memory=False,amount=0):
+    def __init__(self, csv_file, phase='train', in_memory=False,amount=0,tolabel='brand',equallydistributed=False,ignoreLabels=[]):
         self.data_full = pd.read_csv(csv_file)
-        self.classes = sorted(self.data_full['brand'].unique())
-        self.data = self.data_full[self.data_full['phase'] == phase]
 
-        if(amount>0):
-            self.data=self.data.sample(amount)
+        if len(ignoreLabels)>0:
+            self.data_full = self.data_full[~self.data_full[tolabel].isin(ignoreLabels)]
+
+        self.classes = sorted(self.data_full[tolabel].unique())
+        print(f"{len(self.classes)} labels found from column {tolabel}")
+        self.data = self.data_full[self.data_full['phase'] == phase]
+        self.tolabelrow=tolabel
+        self.equalDistribution=equallydistributed
+
+        if equallydistributed:
+            grouped = self.data.groupby(tolabel)
+            min_count = grouped.size().min()
+            self.data = grouped.apply(lambda x: x.sample(min_count)).reset_index(drop=True)
+
+        if amount > 0 and not equallydistributed:
+            self.data = self.data.sample(amount)
 
         self.label_map = {label: idx for idx, label in enumerate(self.classes)}
         self.in_memory = in_memory
@@ -56,7 +67,7 @@ class CustomCarDataset(Dataset):
             futures = []
             with tqdm.tqdm(total=len(rows), desc="Images to RAM", unit='image', dynamic_ncols=True) as tbar:
                 for _, row in rows:
-                    futures.append(executor.submit(load_single_image, row, self.label_map))
+                    futures.append(executor.submit(load_single_image, row, self.label_map,self.tolabelrow))
                 
                 for future in futures:
                     image, label = future.result()
@@ -70,6 +81,6 @@ class CustomCarDataset(Dataset):
         if self.in_memory:
             image, label = self.images[idx]
         else:
-            image, label = load_single_image(self.data.iloc[idx],self.label_map)
+            image, label = load_single_image(self.data.iloc[idx],self.label_map,self.tolabelrow)
 
         return image, label
