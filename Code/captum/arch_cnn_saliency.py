8cd290565473d801d0fab24c012c68f864955eed
JohannesDecker95 <jo.de@online.de>
2024-12-03 12:55:49 +0100

    repo restructured

--- Code/captum/arch_cnn_saliency.py
+++ Code/captum/arch_cnn_saliency.py
@@ -24,6 +24,7 @@ target_layer = model.layer4[2].conv3
 preprocess = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
+    # standard normalization parameters of ImageNet
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
